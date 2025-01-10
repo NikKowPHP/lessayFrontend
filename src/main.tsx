eb31a4a899e3b468c04c68bdc0a140db398bda9a
NikKowPHP <nik.kow@outlook.com>
2025-01-10 13:06:21 +0100

    feat: init store in store provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { store } from './store'
-import { router } from './router'
 import './index.css'
+import AppProvider from './store/providers/AppProvider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <AppProvider />
   </StrictMode>,
 )
